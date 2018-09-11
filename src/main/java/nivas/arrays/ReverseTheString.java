12e3e0fcfb5475193a1abf6fc86747b935c9a41d
Seenivasan Sankaran <seenivasan.s@heymath.com>
2018-09-11 15:29:48 +0530

    Sample Programs added

---
+++ src/main/java/nivas/arrays/ReverseTheString.java
@@ -0,0 +1,21 @@
+package nivas.arrays;
+
+
+//ReverseTheString Using Recursive Method
+
+public class ReverseTheString {
+
+    public static void main(String[] args) {
+        String input = "GeeksforGeeks";
+        byte[] strAsByteArray = input.getBytes();
+        byte[] result = new byte[strAsByteArray.length];
+
+
+        for (int i = 0; i < strAsByteArray.length; i++){
+            result[i] = strAsByteArray[strAsByteArray.length - i - 1];
+        }
+
+        System.out.println(new String(result));
+    }
+
+}
